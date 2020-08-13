65f433b550e4a563dfd5765739877723a107df8f
Gong Deli <gnnnnng@gmail.com>
2020-08-13 23:24:21 +0800

    use AccountData type from upstream pallet-balance

--- src/frame/balances.rs
+++ src/frame/balances.rs
@@ -32,6 +32,7 @@ use sp_runtime::traits::{
     Member,
 };
 use std::fmt::Debug;
+pub use pallet_balances::AccountData;
 
 /// The subset of the `pallet_balances::Trait` that a client must implement.
 #[module]
@@ -46,30 +47,6 @@ pub trait Balances: System {
         + MaybeSerialize
         + Debug
         + From<<Self as System>::BlockNumber>;
-}
-
-/// All balance information for an account.
-#[derive(Clone, Debug, Eq, PartialEq, Default, Decode, Encode)]
-pub struct AccountData<Balance> {
-    /// Non-reserved part of the balance. There may still be restrictions on this, but it is the
-    /// total pool what may in principle be transferred, reserved and used for tipping.
-    ///
-    /// This is the only balance that matters in terms of most operations on tokens. It
-    /// alone is used to determine the balance when in the contract execution environment.
-    pub free: Balance,
-    /// Balance which is reserved and may not be used at all.
-    ///
-    /// This can still get slashed, but gets slashed last of all.
-    ///
-    /// This balance is a 'reserve' balance that other subsystems use in order to set aside tokens
-    /// that are still 'owned' by the account holder, but which are suspendable.
-    pub reserved: Balance,
-    /// The amount that `free` may not drop below when withdrawing for *anything except transaction
-    /// fee payment*.
-    pub misc_frozen: Balance,
-    /// The amount that `free` may not drop below when withdrawing specifically for transaction
-    /// fee payment.
-    pub fee_frozen: Balance,
 }
 
 /// The total issuance of the balances module.
